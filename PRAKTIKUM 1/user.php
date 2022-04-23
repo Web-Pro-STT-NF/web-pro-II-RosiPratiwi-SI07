483975af0fbdcfb612f782ece85ee5154b5a0a09
Rosi Pratiwi <Rossypratiwi013@gmail.com>
2022-04-23 20:34:41 +0700

    add praktikum

---
+++ PRAKTIKUM 1/user.php
@@ -0,0 +1,11 @@
+<?php
+$nama = 'Rosi Pratiwi';
+$umur = 20;
+$berat = 44;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. ' Tahun';
+echo '<br/>berat : ' .$berat. ' Kg';
+
+echo "<br/>Hello $nama Apa kabar? ";
+?>
